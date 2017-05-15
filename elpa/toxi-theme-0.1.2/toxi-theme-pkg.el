9b31546a3699a5f25d9f5f351ba0c2d405bbb039
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-05-15 16:20:54 +0200

    initial release

---
+++ elpa/toxi-theme-0.1.2/toxi-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "toxi-theme" "0.1.2" "A dark color theme by toxi" '((emacs "24")) :url "http://bitbucket.org/postspectacular/toxi-theme/")
---
+++ init.el
@@ -0,0 +1,54 @@
+;; Add Melpa archive
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (toxi-theme color-theme-sanityinc-tomorrow zenburn-theme ace-window beacon elpy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; suppress menubar and X toolbar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; add column number
+(setq column-number-mode t)
+
+;; save open files configuration
+(desktop-save-mode 1)
+
+;; auto reload changed buffers
+(global-auto-revert-mode t)
+
+;; activate parentheses matching mode
+(show-paren-mode 1)
+
+;; python venv
+(setq pyvenv-activate "~/pyenv")
+
+;; Beacon configuration (cursor localization)
+(beacon-mode 1)
+
+;; ace-window config (quickly switch between windows)
+(global-set-key (kbd "M-m") 'ace-window)
+
+;; Activate dark low contrast zenburn-theme
+;(load-theme 'zenburn t)
+(load-theme 'toxi t)
