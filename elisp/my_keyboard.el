343176ae38050490a8dc32da2115744dd57abafd
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-10-14 17:17:14 +0200

    commit.txt

--- elisp/my_keyboard.el
+++ elisp/my_keyboard.el
@@ -1,1 +1,6 @@
+; Standard C-z undo instead of M-/
 (global-set-key (kbd "C-z") 'undo)
+
+; For OSX unmap right alt key for OSX specific usage
+(when (eq system-type 'darwin)
+  (setq mac-right-option-modifier 'none))
