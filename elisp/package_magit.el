3560a85e2ceed7aeb7f78cbca909b6b0245fb38a
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-10-14 22:36:53 +0200

    correct syntax error in package_magit.el

--- elisp/package_magit.el
+++ elisp/package_magit.el
@@ -1,6 +1,8 @@
+(require 'magit)
+
 ;; magit config
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; set git executable fir OSX
 (when (eq system-type 'darwin)
-  (setq magit-git-executable "/usr/local/bin/git")
+  (setq magit-git-executable "/usr/local/bin/git"))
