e25b758b8823bc9d2c04ae77eebd3372b3eb4701
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-10-23 16:07:10 +0200

    add ssh proxy in my_tramp.el for *.ermont hosts

--- elisp/my_tramp.el
+++ elisp/my_tramp.el
@@ -3,3 +3,5 @@
 	     '("\\.cfa\\.life\\'" "\\`root\\'" "/ssh:%h:"))
 (add-to-list 'tramp-default-proxies-alist
 	     '("\\.home\\'" "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+	     '("\\.ermont\\'" "\\`root\\'" "/ssh:%h:"))
