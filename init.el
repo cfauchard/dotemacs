89227746fdb2c55d380cfee3a0c43343d307f0dd
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-11-07 10:57:05 +0100

    add wikipedia-mode

--- init.el
+++ init.el
@@ -22,4 +22,22 @@
 (load-library "package_magit")
 (load-library "package_beacon")
 (load-library "package_ace-window")
-
+(load-library "package_wikipedia-mode")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" default)))
+ '(package-selected-packages
+   (quote
+    (## multi-term magit toxi-theme zenburn-theme ace-window beacon elpy)))
+ '(send-mail-function (quote mailclient-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mouse ((t (:background "white")))))
