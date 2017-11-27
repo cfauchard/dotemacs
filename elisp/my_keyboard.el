7d44bcfc7ce5f0db12e1c69cb793aa36bb0b7478
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-11-27 11:37:48 +0100

    ajout raccourci C-x M-b pour renommer le buffer, ajout config tramp pour pi

--- elisp/my_keyboard.el
+++ elisp/my_keyboard.el
@@ -1,6 +1,9 @@
 ; Standard C-z undo instead of M-/
 (global-set-key (kbd "C-z") 'undo)
 
+; Rename buffer
+(global-set-key (kbd "C-x M-b") 'rename-buffer)
+
 ; For OSX unmap right alt key for OSX specific usage
 (when (eq system-type 'darwin)
   (setq mac-right-option-modifier 'none))
--- elisp/my_tramp.el
+++ elisp/my_tramp.el
@@ -1,3 +1,5 @@
+;; with proxies configurations bellow, sudo file open on remote host will first
+;; execute ssh connection, and then a sudo
 (require 'tramp)
 (add-to-list 'tramp-default-proxies-alist
 	     '("\\.cfa\\.life\\'" "\\`root\\'" "/ssh:%h:"))
@@ -5,3 +7,7 @@
 	     '("\\.home\\'" "\\`root\\'" "/ssh:%h:"))
 (add-to-list 'tramp-default-proxies-alist
 	     '("\\.ermont\\'" "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+	     '("pi\\.intranet\\.itnovem\\.com\\'" "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+	     '("PI-SYS-NET\\'" "\\`root\\'" "/ssh:%h:"))
